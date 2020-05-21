00c3d0b8687ae2aebff60d88fd86748162c01a1e
Cody Green <cody@codygreen.com>
2020-05-21 12:57:47 -0500

    check DNS

---
+++ labs/lab0/do-ready/controls/do-ready.rb
@@ -0,0 +1,17 @@
+# copyright: 2020, F5 Networks
+
+title "Declarative Onboarding Ready"
+
+# Check DNS
+control "DNS Configured" do
+  impact 1.0
+  title "DNS Set to 8.8.8.8 and 8.8.4.4"
+  desc "Ensure that the correct DNS is set"
+
+  describe command("tmsh show sys ip-address") do
+    its("stdout") { should contain "8.8.8.8")}
+    its("stdout") { should contain "8.8.4.4")}
+  end
+end
+
+
