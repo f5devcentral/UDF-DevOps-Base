77f1741b61d1f1510466420eebb6d2dec4046f43
codygreen <cody@codygreen.com>
2020-09-10 04:55:02 +0000

    fix bigip inspec tests

--- setup/runtests.sh
+++ setup/runtests.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
-as3_version=3.22.0-2
-do_version=1.15.0-3
+
+echo "------------ TESTING BIGIPS ------------"
+
+as3_version=3.22.0
+do_version=1.15.0
+ts_version=1.14.0
+fast_version=1.3.0
 for i in {6..7} 
 do
-    inspec exec tests/bigips \
-    --input bigip_address=10.1.1.$i password=$BIGIP_PWD do_version=$do_version \
-    as3_version=$as3_version
+    inspec exec tests/bigips --input bigip_address=10.1.1.6 password=$BIGIP_PWD \
+    do_version=$do_version as3_version=$as3_version ts_version=$ts_version \
+    fast_version=$fast_version
 done
+
+echo "------------ TESTING APP SERVERS ------------"
+inspec exec tests/appservers
+
+echo "------------ TESTING CLIENT SERVER ------------"
+inspec exec tests/clientserver
--- setup/tests/bigips/controls/bigips.rb
+++ setup/tests/bigips/controls/bigips.rb
@@ -1,6 +1,12 @@
 title "BIG-IP Ready"
 
-include_controls 'big-ip-atc-ready' do
-  skip_control 'bigip-telemetry-streaming'
-  skip_control 'bigip-telemetry-streaming-version'
+require_controls 'big-ip-atc-ready' do
+  control 'bigip-connectivity'
+  control 'bigip-declarative-onboarding'
+  control 'bigip-declarative-onboarding-version'
+  control 'bigip-application-services'
+  control 'bigip-application-services-version'
+  control 'bigip-telemetry-streaming'
+  control 'bigip-telemetry-streaming-version'
+  control 'bigip-licensed'
 end
